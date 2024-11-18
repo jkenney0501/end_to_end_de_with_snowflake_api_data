42b2d2963bff52a3adff86a5babffdcd8bc3ca56
James <james.kenney@colorado.edu>
2024-11-18 15:51:00 -0500

    added comments in stage layer section

--- 01-sql-scripts/01-db-schema-wh-ddl.sql
+++ 01-sql-scripts/01-db-schema-wh-ddl.sql
@@ -1,7 +1,5 @@
--- use sysadmin role.
-use role sysadmin;
+-- create teh db's, schemas and wh's needed for the threee layers we will use.
 
--- create a warehoue before you try this.
 
 -- create development database/schema  if does not exist
 create database if not exists dev_db;
@@ -10,21 +8,25 @@ create schema if not exists dev_db.clean_sch;
 create schema if not exists dev_db.consumption_sch;
 create schema if not exists dev_db.publish_sch;
 
+
+-- see all your schemas
 show schemas in database dev_db;
--- visit the object explorer home page from webui.
+
+-- visit the object explorer home page from web-ui.
 
 -- having load_wh warehouse
 create warehouse if not exists load_wh
      comment = 'this is load warehosue for loading all the JSON files'
      warehouse_size = 'medium' 
      auto_resume = true 
-     auto_suspend = 60 
-     enable_query_acceleration = false 
-     warehouse_type = 'standard' 
+     auto_suspend = 60  -- shuts down after 1 minute
+     enable_query_acceleration = false  -- this is fr large scan using serverless compute
+     warehouse_type = 'standard'   -- other option is snowpark-optimized
      min_cluster_count = 1 
      max_cluster_count = 1 
-     scaling_policy = 'standard'
-     initially_suspended = true;
+     scaling_policy = 'standard' -- will favor multi clister over credit consumption, other option is economy which favors ccc over mc.
+     initially_suspended = true; -- starts in suspend mode
+
 
 -- all the ETL workload will be manage by it.
 create warehouse if not exists transform_wh
@@ -39,12 +41,13 @@ create warehouse if not exists transform_wh
      scaling_policy = 'standard'
      initially_suspended = true;
 
+
 -- specific virtual warehouse with differt resume time (for streamlit, it should be longer)
  create warehouse if not exists streamlit_wh
-     comment = 'this is streamlit virtua warehouse' 
+     comment = 'this is streamlit virtual warehouse' 
      warehouse_size = 'x-small' 
      auto_resume = true
-     auto_suspend = 600 
+     auto_suspend = 300 
      enable_query_acceleration = false 
      warehouse_type = 'standard' 
      min_cluster_count = 1 
@@ -52,7 +55,8 @@ create warehouse if not exists transform_wh
      scaling_policy = 'standard'
      initially_suspended = true;
 
--- having adhoc warehouse
+
+-- dhoc warehouse, most dev will use this
 create warehouse if not exists adhoc_wh
      comment = 'this is adhoc warehosue for all adhoc & development activities' 
      warehouse_size = 'x-small' 
@@ -65,5 +69,9 @@ create warehouse if not exists adhoc_wh
      scaling_policy = 'standard'
      initially_suspended = true;
 
+
+
+-- see your wh's
 show warehouses;
--- visit the warehouse home page from webui.
+
+-- also, visit the warehouse home page from webui.
--- 01-sql-scripts/02-stage-layer-ddl-dml.sql
+++ 01-sql-scripts/02-stage-layer-ddl-dml.sql
@@ -1,5 +1,18 @@
+/**************************************************************************************************
+Summmary:
+
+This step loads the data for each hour manually to the internal stage "raw_stg".
+
+From here a task is set up to run every hour to mimick the API data loading every hour.
+
+The task will load the raw data from the interanl stage in to the raw_aql table in a format that is 
+readable but also adds a few audit columns for file/file location and load time.
+
+From here, the clean layer will flatten the data out and prepare it for consumption.
+
+*****************************************************************************************************/
+
 -- change context
-use role sysadmin;
 use schema dev_db.stage_sch;
 use warehouse adhoc_wh;
 
@@ -11,26 +24,33 @@ comment = 'all the air quality raw data will store in this internal stage locati
 
 
  -- create file format to process the JSON file
-  create file format if not exists json_file_format 
-      type = 'JSON'
-      compression = 'AUTO' 
-      comment = 'this is json file format object';
+create file format if not exists json_file_format 
+type = 'JSON'
+compression = 'AUTO' 
+comment = 'this is json file format object';
 
 
-  show stages;
-  list @raw_stg;
+show stages;
+list @raw_stg;
 
-  -- load the data that has been downloaded manually
-  -- run the list command to check it
 
-  -- level-1
+
+-- load the data that has been downloaded manually
+-- run the list command to check it
+list @raw_stg;
+
+
+-- level-1, apply file format to be able t query the data directly from the stage
 select 
+
     * 
 from 
     @dev_db.stage_sch.raw_stg
     (file_format => JSON_FILE_FORMAT) t;
 
-  -- JSON file analysis using json editor
+
+
+  -- JSON file analysis using json editor - query the data
   -- level-2
     select 
         Try_TO_TIMESTAMP(t.$1:records[0].last_update::text, 'dd-mm-yyyy hh24:mi:ss') as index_record_ts,
@@ -40,13 +60,15 @@ from
     from @dev_db.stage_sch.raw_stg
     (file_format => JSON_FILE_FORMAT) t;
 
+
+
 -- level3
 select 
     Try_TO_TIMESTAMP(t.$1:records[0].last_update::text, 'dd-mm-yyyy hh24:mi:ss') as index_record_ts,
     t.$1,
     t.$1:total::int as record_count,
     t.$1:version::text as json_version,
-    -- meta data information
+    -- meta data information for files (always goes in stage)
     metadata$filename as _stg_file_name,
     metadata$FILE_LAST_MODIFIED as _stg_file_load_ts,
     metadata$FILE_CONTENT_KEY as _stg_file_md5,
@@ -54,6 +76,8 @@ select
 
 from @dev_db.stage_sch.raw_stg
 (file_format => JSON_FILE_FORMAT) t;
+
+
 
 -- @todo interview tag...
 -- could you explain the metadata properties
@@ -77,13 +101,16 @@ create or replace transient table raw_aqi (
     _copy_data_ts timestamp default current_timestamp()
 );
 
+-- see table metadata
+describe table raw_aqi;
+
+-- Questions for thought:
 -- should you create transient table or permanent table? if so why?
 -- how the standard table cost more with fail safe concept?
 
+
+
 -- copy command
-
-
-
 -- following copy command
 create or replace task copy_air_quality_data
     warehouse = load_wh
@@ -106,10 +133,12 @@ copy into raw_aqi (index_record_ts,json_data,record_count,json_version,_stg_file
 file_format = (format_name = 'dev_db.stage_sch.JSON_FILE_FORMAT') 
 ON_ERROR = ABORT_STATEMENT; 
 
+show tasks; -- starts in suspend mode
 
-use role accountadmin;
-grant execute task, execute managed task on account to role sysadmin;
-use role sysadmin;
+
+--use role accountadmin;
+--grant execute task, execute managed task on account to role sysadmin;
+--use role sysadmin;
 
 alter task dev_db.stage_sch.copy_air_quality_data resume;
 
